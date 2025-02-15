d2ef384c710aa6299a158dfe227a6009286f26b0
Arpit Patel <patelarpit2580@gmail.com>
2025-02-15 11:58:15 +0530

    resolve

--- src/firebase/Firebase.jsx
+++ src/firebase/Firebase.jsx
@@ -1,18 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB6yjQIm_ny-bHyeBC-O3EGyOGpiwWcOI4",
   authDomain: "e-commerce-fdfc9.firebaseapp.com",
   databaseURL: "https://e-commerce-fdfc9-default-rtdb.firebaseio.com",
   projectId: "e-commerce-fdfc9",
-  storageBucket: "e-commerce-fdfc9.firebasestorage.app",
+  storageBucket: "e-commerce-fdfc9.appspot.com", 
   messagingSenderId: "430636310650",
   appId: "1:430636310650:web:d1c1fcddaa2934d79fdbc3",
   measurementId: "G-6FMSESSH3H"
@@ -20,7 +15,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app); 
+
+// Initialize Analytics (Optional)
 const analytics = getAnalytics(app);
-export const auth=getAuth();
 
 export default app;
