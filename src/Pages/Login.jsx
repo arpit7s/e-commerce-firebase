c045d59749eb5c8b1948d2b20b9466bec9198982
Arpit Patel <patelarpit2580@gmail.com>
2025-02-25 10:15:56 +0530

    done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { auth } from "../firebase/Firebase";
+import { Link, useNavigate } from "react-router-dom";;
+import { auth } from "../Firebase/Firebase";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
